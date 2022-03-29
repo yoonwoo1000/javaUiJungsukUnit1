d7758702b5e6827aad02f0ff656ab28feae27425
YoonWoo <yoonwoo1000@gmail.com>
2022-03-29 18:46:34 +0900

    Initial commit

---
+++ src/StringEx.java
@@ -0,0 +1,17 @@
+public class StringEx {
+    public static void main(String[] args){
+        String name = "ja" + "va";
+        String str = name + 8.0;
+
+        System.out.println(name);
+        System.out.println(str);
+        System.out.println(7 + " ");
+        System.out.println(" " + 7);
+        System.out.println(7 + "");
+        System.out.println("" + 7);
+        System.out.println("" + "");
+        System.out.println(7 + 7 + "");
+        System.out.println("" + 7 + 7);
+
+    }
+}
