bae34c25fd8f3acab079e14a55caeaf499048cf8
YoonWoo <yoonwoo1000@gmail.com>
2022-03-30 18:21:04 +0900

    Unit1 done

---
+++ src/CastingEx.java
@@ -0,0 +1,9 @@
+public class CastingEx {
+    public static void main(String[] args){
+        double d = 85.4;
+        int score = (int)d;
+
+        System.out.println("score = " +score);
+        System.out.println("d=" +d);
+    }
+}
---
+++ src/CatingEx3.java
@@ -0,0 +1,11 @@
+public class CatingEx3 {
+    public static void mian(String[] args){
+        float f = 9.123456f;
+        double d = 9.1234567;
+        double d2 = (double)f;
+
+        System.out.printf("f = %20.18f\n", f);
+        System.out.printf("d = %20.18f\n", d);
+        System.out.printf("d2= %20.18f\n", d2);
+    }
+}
